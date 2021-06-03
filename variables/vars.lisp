ea128911e0a6ba43154b1b0ab28d815c4357d23d
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-06-02 21:16:07 -0400

    Omochroi

--- variables/vars.lisp
+++ variables/vars.lisp
@@ -1,6 +1,6 @@
 (+ 1 1)
 
-(car (list #'(lambda() 1)))
+(first (list #'(lambda() 1)))
 
 (third 
   (let ((count 0))
@@ -8,3 +8,43 @@
       #'(lambda () (incf count))
       #'(lambda () (decf count))
       #'(lambda () count)))) ; => Still dont know how to invoke these ...
+
+(defvar *x* 10)
+(defun foo () (format t "X: ~d~%" *x*))
+
+(foo)
+
+;;; CL-USER(4): (defun foo () (format t "X: ~d~%" *x*))
+;;; 
+;;; FOO
+;;; CL-USER(5): (foo)
+;;; X: 10
+;;; NIL
+;;; CL-USER(6): (let ((*x* 20)) (foo))
+;;; X: 20
+;;; NIL
+;;; CL-USER(7): (foo)
+;;; X: 10
+;;; NIL
+;;; CL-USER(8):
+
+(defun bar ()
+  (foo)
+  (let ((*x* 20)) (foo))
+  (foo))
+
+(bar)
+
+;;; CL-USER(13): (defun bar ()
+;;;                (foo)
+;;;                (let ((*x* 20)) (foo))
+;;;                (foo))
+;;; WARNING: redefining COMMON-LISP-USER::BAR in DEFUN
+;;; 
+;;; BAR
+;;; CL-USER(14): (bar)
+;;; X: 10
+;;; X: 20
+;;; X: 10
+;;; NIL
+;;; CL-USER(15)
