375537c0db8369e3d09fb31bc8786064e06d02aa
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-06-08 19:46:06 -0400

    pfeh *insatisfaction sounds*

---
+++ 12-list-process/lisp.lisp
@@ -0,0 +1,6 @@
+(defparameter condf (list 4 5 2 2 3))
+
+(print condf) 
+
+(nsubstitute 78 2 condf)
+
---
+++ files/files.lisp
@@ -0,0 +1,39 @@
+(+ 1 1)
+
+(with-open-file (fDesc "/home/zenandroid/fii.txt" :if-exists :supersede :direction :output)
+  (format fDesc "Heeeeeeey"))
+
+(make-pathname :name "foo" :type "txt")
+
+(pprint *DEFAULT-PATHNAME-DEFAULTS*)
+
+(defun component-present-p (value)
+  (and value (not (eql value :unspecific))))
+
+(defun directory-pathname-p  (p)
+  (and
+   (not (component-present-p (pathname-name p)))
+   (not (component-present-p (pathname-type p)))
+   p))
+
+(defun pathname-as-directory (name)
+  (let ((pathname (pathname name)))
+    (when (wild-pathname-p pathname)
+      (error "Can't reliably convert wild pathnames."))
+    (if (not (directory-pathname-p name))
+      (make-pathname
+       :directory (append (or (pathname-directory pathname) (list :relative))
+                          (list (file-namestring pathname)))
+       :name      nil
+       :type      nil
+       :defaults pathname)
+      pathname)))
+
+(defun directory-wildcard (dirname)
+  (make-pathname
+   :name :wild
+   :type #-clisp :wild #+clisp nil
+   :defaults (pathname-as-directory dirname)))
+
+;; Gotta be honest mang, this stuff isnt getting in the noggin too much ...
+;; Too abstract and shit
