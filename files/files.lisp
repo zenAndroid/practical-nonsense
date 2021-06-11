d18dec8fea511c6f7b9844797c9da80f043cc437
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-06-10 22:23:42 -0400

    Mmm, okay i feel like my umderstanding is better, but still some brain compactification needed

--- files/files.lisp
+++ files/files.lisp
@@ -1,14 +1,9 @@
-(+ 1 1)
-
-(with-open-file (fDesc "/home/zenandroid/fii.txt" :if-exists :supersede :direction :output)
-  (format fDesc "Heeeeeeey"))
-
 (defparameter paname (make-pathname :directory '(:absolute "foo" "bar") :name "help"))
 
 (print *DEFAULT-PATHNAME-DEFAULTS*)
 
-(defun component-present-p (value)
-  (and value (not (eql value :unspecific))))
+(defun component-present-p (value);{{{
+  (and value (not (eql value :unspecific))));}}}
 
 (defun directory-pathname-p  (p);{{{
   (and
@@ -37,11 +32,103 @@
    :type #-clisp :wild #+clisp nil
    :defaults (pathname-as-directory dirname)));}}}
 
+;; ;; ;; ;; (defun list-directory (dirname);{{{
+;; ;; ;; ;;   "Tries \"guarding\" the path name then calling the directory function on it"
+;; ;; ;; ;;   (when (wild-pathname-p dirname)
+;; ;; ;; ;;     (error "Can only list concrete directory names."))
+;; ;; ;; ;;   (directory (directory-wildcard dirname)));}}}
+
+(defun list-directory (dirname);{{{
+  (when (wild-pathname-p dirname)
+    (error "Can only list concrete directory names."))
+  (let ((wildcard (directory-wildcard dirname)))
+
+    ; Once you get all the implementations returning directories, you'll discover
+    ; they can also differ in whether they return the names of directories in
+    ; directory or file form. You want list-directory to always return directory
+    ; names in directory form so you can differentiate subdirectories from
+    ; regular files based on just the name. Except for Allegro, all the
+    ; implementations this library will support do that. Allegro, on the other
+    ; hand, requires you to pass DIRECTORY the implementation-specific keyword
+    ; argument :directories-are-files NIL to get it to return directories in file
+    ; form.
+
+    #+(or sbcl cmu lispworks)
+    (directory wildcard)
+
+    #+openmcl
+    (directory wildcard :directories t)
+
+    #+allegro
+    (directory wildcard :directories-are-files nil)
+
+    #+clisp
+    (nconc
+     (directory wildcard)
+     (directory (clisp-subdirectories-wildcard wildcard)))
+
+    #-(or sbcl cmu lispworks openmcl allegro clisp)
+    (error "list-directory not implemented")));}}}
+
+#+clisp
+(defun clisp-subdirectories-wildcard (wildcard);{{{ Pretty much just adds :wild to the directory component
+  (make-pathname
+   :directory (append (pathname-directory wildcard) (list :wild))
+   :name nil
+   :type nil
+   :defaults wildcard));}}}
+
+(defun file-exists-p (pathname);{{{
+  #+(or sbcl lispworks openmcl)
+  (probe-file pathname)
+
+  #+(or allegro cmu)
+  (or (probe-file (pathname-as-directory pathname))
+      (probe-file pathname))
+
+  #+clisp
+  (or (ignore-errors
+        (probe-file (pathname-as-file pathname)))
+      (ignore-errors
+        (let ((directory-form (pathname-as-directory pathname)))
+          (when (ext:probe-directory directory-form)
+            directory-form))))
+
+  #-(or sbcl cmu lispworks openmcl allegro clisp)
+  (error "file-exists-p not implemented"));}}}
+
+(defun pathname-as-file (name);{{{
+  (let ((pathname (pathname name)))
+    (when (wild-pathname-p pathname)
+      (error "Can't reliably convert wild pathnames."))
+    (if (directory-pathname-p name)
+      (let* ((directory (pathname-directory pathname))
+             (name-and-type (pathname (first (last directory)))))
+        (make-pathname
+         :directory (butlast directory)
+         :name (pathname-name name-and-type)
+         :type (pathname-type name-and-type)
+         :defaults pathname))
+      pathname)));}}}
+
+(defun walk-directory (dirname fn &key directories (test (constantly t)));{{{
+  (labels
+      ((walk (name)
+         (cond
+           ((directory-pathname-p name)
+            (when (and directories (funcall test name))
+              (funcall fn name))
+            (dolist (x (list-directory name)) (walk x)))
+           ((funcall test name) (funcall fn name)))))
+    (walk (pathname-as-directory dirname)))) ;}}}
+
 (merge-pathnames #p"foo/bar.html" #p"/www/html/")
 
 ;; It finally clicking, ... nothing better than pen and paper when the confusion strikes ...
 
 paname ; #P"/foo/bar/help"
+
+(pathname-type (pathname (first (last (pathname-directory paname)))))
 
 (pathname-as-directory paname) ; This helps me understand what is going on this results in #P"/foo/bar/help/"
 
