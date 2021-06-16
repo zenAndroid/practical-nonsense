689ff84ae7fa7bb5421f149e6144b1508d86f800
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-06-16 18:01:16 -0400

    Lol, ok im over it

--- special-ops/special.lisp
+++ special-ops/special.lisp
@@ -1,1 +1,15 @@
-(labels ((foobar (b) (format nil "~a" b))) (foobar 45))  ; 45
+(labels ((foobar (b) (format nil "~a" b)) 
+         (second-func (a b) (* a b))
+         (third-foo (b) (if (= b 0) (foobar "boop") (second-func 55 2))))
+  (third-foo 4))  ; 45
+
+(block balook
+  (format t "~a~%" 'kke)
+  (return-from balook '(bepis))
+  (format t "~a" 'kek))
+  
+(tagbody
+ a (print 'a) (if (zerop (random 2)) (go c))
+ b (print 'b) (if (zerop (random 2)) (go a))
+ c (print 'c) (if (zerop (random 2)) (go b)))
+
